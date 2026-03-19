4a0a1c6c635589c7511c1866074dd06edb218060
danmelon <toxicmelondan@gmail.com>
2026-03-19 22:43:26 +0000

    some function scatter plots

--- scatterplots.py
+++ scatterplots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 
-## Single Points
+
 plt.style.use('dark_background') ## I put this in because I prefer darkmode
 '''
 other styles = default, classic, Solarize_Light2, bmh, fast, fivethirtyeight, ggplot, greyscale, petroff10,... etc...
@@ -14,7 +14,20 @@ x_data = np.random.randint(0, 100, (500, ))
 y_data = np.random.randint(0, 100, (500, ))
 z_data = np.random.randint(0, 100, (500, ))
 
-ax.scatter(x_data, y_data, z_data)
+ax.scatter(x_data, y_data, z_data, color ='hotpink')
 
 plt.show()
 
+trig = plt.axes(projection="3d")
+x = np.arange(0, 50, 0.1)
+y = np.arange(0, 50, 0.1)
+z = np.sin(x) * np.cos(y)
+
+trig.scatter(x, y, z, color="hotpink")
+trig.set_title("function scatter")
+trig.set_xlabel("x values")
+trig.set_ylabel("y values")
+trig.set_zlabel("z values")
+
+
+plt.show()
