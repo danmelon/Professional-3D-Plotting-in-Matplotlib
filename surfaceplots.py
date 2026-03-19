e2b2efad2251bb9cb8d0092771624ad823dcccd0
danmelon <toxicmelondan@gmail.com>
2026-03-19 22:58:16 +0000

    surface plots using np meshgrid and cmap

---
+++ surfaceplots.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+plt.style.use('dark_background') ## I put this in because I prefer darkmode
+
+ax = plt.axes(projection="3d")
+
+x_data = np.arange(0, 50, 0.1)
+y_data = np.arange(0, 50, 0.1)
+
+X, Y = np.meshgrid(x_data, y_data)
+
+Z = X * Y
+
+ax.plot_surface(X, Y, Z)
+plt.show()
+
+sine = plt.axes(projection="3d")
+
+x = np.arange(-5, 5, 0.1)
+y = np.arange(-5, 5, 0.1)
+
+XX, YY = np.meshgrid(x, y)
+
+ZZ = np.sin(XX) * np.cos(YY)
+
+sine.plot_surface(XX, YY, ZZ, cmap = "hsv")
+plt.show()
